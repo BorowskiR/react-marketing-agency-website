21ab58b86a54cd3fba8ebf53f949c72b22f4a789
Rafal Borowski <rafall.borowski@gmail.com>
2020-11-08 13:47:43 +0100

    Navbar

---
+++ src/globalStyles.js
@@ -0,0 +1,27 @@
+import styled, { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        font-family: 'Source Sans Pro', sans-serif;
+    }
+`;
+
+export const Container = styled.div`
+  z-index: 1;
+  width: 100%;
+  max-width: 1300px;
+  margin-right: auto;
+  margin-left: auto;
+  padding-left: 50px;
+  padding-right: 50px;
+
+  @media screen and (max-width: 991px) {
+    padding-left: 30px;
+    padding-right: 30px;
+  }
+`;
+
+export default GlobalStyle;
